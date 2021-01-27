6c0297021ad29cf0b7dab1c0a7ded77865fafac3
vsshk <v.s.shkodin@gmail.com>
2021-01-26 23:34:06 -0800

    assertion added

--- src/SimpleTest.java
+++ src/SimpleTest.java
@@ -2,6 +2,7 @@ package src.SimpleTest;
 
 
 import org.testng.annotations.*;
+import org.testng.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -11,9 +12,12 @@ public class SimpleTest {
 	 public void aFastTest() {
 
 	  	// Optional. If not specified, WebDriver searches the PATH for chromedriver.
-	    System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+	    System.setProperty("webdriver.chrome.driver", "C:/tools/chromedriver.exe");
 	    WebDriver driver = new ChromeDriver();
 	    driver.get("http://www.google.com/");
+	    String expectedTitle = "Google";
+		String originalTitle = driver.getTitle();
+		Assert.assertEquals(originalTitle, expectedTitle);
 	    driver.quit();
 
   		}
